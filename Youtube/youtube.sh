c28276e17d0fa5451f0f3bb7aff54e2af4024cf6
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2019-05-21 14:39:38 -0400

    Update youtube.sh

--- Youtube/youtube.sh
+++ Youtube/youtube.sh
@@ -6,7 +6,7 @@
 #license: MIT License
 
 function youtube(){
-    (
+	(
 
     #Cria a variavel _video em uma pasta temporaria com o comando "mktemp"
     _video=$(mktemp)
@@ -21,7 +21,7 @@ function youtube(){
 
     #Comando wget, 1 sera pego no final do codigo como parametro, -O (maisculo) vai jogar o arquivo na variavel _video, modo silencioso
     wget "$_a" -O "$_video" 2>/dev/null
-    
+
     #Cria as variaveis com os comandos 
     _title=$(grep '<title>' "$_video" | sed 's/<[^>]*>//g' | sed 's/ - You.*//g')
     _viesw=$(grep 'watch-view-count' "$_video" | sed 's/<[^>]*>//g')
@@ -30,13 +30,13 @@ function youtube(){
     _id=$(sed 's/channel/\n&/g' "$_video" | grep '^channelId' |sed -n 1p | sed 's/isCrawlable.*//g;s/..,.*//g;s/.*"//g')
     _subscriber=$(sed -n '/subscriber-count/{p; q;}' "$_video" |sed 's/.*subscriber-count//g' | sed 's/<[^>]*>//g;s/.*>//g')
     _descricao=$(grep 'watch-description-text' "$_video" |sed 's/.*id="watch-description-text" class=""//g' |sed 's/<[^>]*>//g' |cut -c1-100 |sed 's/>//g')
-    
+
     #Comando wget juntas as variaveis _url, _id e joga a juncao na variavel _channel
     wget "$_url/$_id" -O "$_channel" 2>/dev/null
 
     #Variavel comando  
     _tchannel=$(sed -n '/title/{p; q;}' "$_channel" | sed 's/<title> //g')
-        
+
     #Porcentagem da Barra de progresso Zenity
     echo "5"
     echo "# Make variables..." ; sleep 1
@@ -52,20 +52,20 @@ function youtube(){
 
     #Barra de progresso Zenity
     ) |
-    zenity --progress \
-      --title="Download YouTube" \
-      --text="Loading..." \
-      --percentage=0 --width="250" --height="50"
+	    zenity --progress \
+	    --title="Download YouTube" \
+	    --text="Loading..." \
+	    --percentage=0 --width="250" --height="50"
 
     #Tratamento de erro
     if [ "$?" = -1 ] ; then
-            zenity --error \
-            --text="Download canceled."
+	    zenity --error \
+		    --text="Download canceled."
     fi
-        
+
 
     clear
-      
+
 }
 
 #Chama a funcao youtube
