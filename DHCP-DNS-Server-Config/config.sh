1f8a317cc00ab7e9e4e93399e923ff73c619bb4f
Karan Luciano <32453979+lkaranl@users.noreply.github.com>
2019-05-21 15:31:30 -0400

    Update config.sh

--- DHCP-DNS-Server-Config/config.sh
+++ DHCP-DNS-Server-Config/config.sh
@@ -55,17 +55,61 @@ Rede(){
 		read gate6
 		dispo=$(ip a |grep enp |awk '{print $2}' |head -n 1)
 		aux=$(ls /etc/netplan/)
-		echo -e "network:
-		ethernets:
-		$dispo
-		dhcp4: false
-		dhcp6: false
-		addresses: [$ip4, \"$ip6\"]
-		gateway4: $gate4
-		gateway6: $gate6
-		version: 2" > /etc/netplan/$aux	
+		echo -e "network:\n   ethernets:\n      $dispo\n         dhcp4: false\n         dhcp6: false\n         addresses: [$ip4, \"$ip6\"]\n         gateway4: $gate4\n         gateway6: $gate6\n   version: 2" > /etc/netplan/$aux	
 		netplan apply
 	fi
+	Menu
+}
+
+Hostname(){
+	hostname=$(cat /etc/hostname)
+	aux6=$(echo $_ip6    $hostname.ubuntu.local    $hostname)
+	sed -i "1s/^/$aux6\n/" /etc/hosts
+	aux4=$(echo $_ip4    $hostname.ubuntu.local    $hostname)
+	sed -i "1s/^/$aux4\n/" /etc/hosts
+	echo -e '\033[05;31mADICIONADO, REINICIE O SEU COMPUTADOR PARA QUE A MUDANCA TENHA EFEITO\033[00;37m' 
+	Menu 
+}
+
+Scopo(){
+	echo -e "#CRIANDO SCOPO PARA DHCP\n#Subnet adicionado pelo Sript. By: Karan\n#option domain-name 'example.org';\noption domain-name-servers ns1.example.org, ns2.example.org;\ndefault-lease-time 600;\nmax-lease-time 7200;\nddns-update-style none;\nauthoritative;" > /etc/dhcp/dhcpd.conf
+	echo "Diga a REDE: "
+	read rede
+	echo "Diga a MASCARA: "
+	read mask
+	echo "Diga quando o RANGE comeca: "
+	read rangec
+	echo "Diga quando o RANGE termina: "
+	read ranget
+	echo -e "\n#RESERVAS IPV4\nsubnet $rede netmask $mask {\nrange $rangec $ranget;\n}" >> /etc/dhcp/dhcpd.conf
+	clear
+	/etc/init.d/isc-dhcp-server restart
+	clear
+	/etc/init.d/isc-dhcp-server status
+	Menu
+}	
+
+Reserva(){
+	echo -e '\033[05;31mCRIANDO RESERVAS\033[00;37m'   
+	echo "Diga o MAC: "
+	read mac
+	if [[ $mac =~ ^[0-9A-Fa-f]{12}$ ]]
+	then
+		echo -e "MAC Valido"
+	else
+		clear
+		echo -e "MAC Invalido"
+		Reserva
+	fi
+	mac=$(echo $mac | sed -e 's!\.!!g;s!\(..\)!\1:!g;s!:$!!' -e 'y/abcdef/ABCDEF/')
+	echo  "Diga o nome do HOST: "
+	read host
+	echo "Diga o IP: "
+	read ip
+	echo -e "\nhost $host {\nhardware ethernet $mac;\nfixed-address $ip;\n}" >> /etc/dhcp/dhcpd.conf
+	/etc/init.d/isc-dhcp-server restart
+	clear
+	/etc/init.d/isc-dhcp-server status
 	Menu
 }
 
@@ -121,57 +165,5 @@ Adicionar(){
 	
 	/etc/init.d/bind9 restart
 	Menu
-}
-
-Hostname(){
-	hostname=$(cat /etc/hostname)
-	aux6=$(echo $_ip6    $hostname.ubuntu.local    $hostname)
-	sed -i "1s/^/$aux6\n/" /etc/hosts
-	aux4=$(echo $_ip4    $hostname.ubuntu.local    $hostname)
-	sed -i "1s/^/$aux4\n/" /etc/hosts
-	echo -e '\033[05;31mADICIONADO, REINICIE O SEU COMPUTADOR PARA QUE A MUDANCA TENHA EFEITO\033[00;37m' 
-	Menu 
-}
-
-Scopo(){
-	echo -e "#CRIANDO SCOPO PARA DHCP\n#Subnet adicionado pelo Sript. By: Karan\n#option domain-name 'example.org';\noption domain-name-servers ns1.example.org, ns2.example.org;\ndefault-lease-time 600;\nmax-lease-time 7200;\nddns-update-style none;\nauthoritative;" > /etc/dhcp/dhcpd.conf
-	echo "Diga a REDE: "
-	read rede
-	echo "Diga a MASCARA: "
-	read mask
-	echo "Diga quando o RANGE comeca: "
-	read rangec
-	echo "Diga quando o RANGE termina: "
-	read ranget
-	echo -e "\n#RESERVAS IPV4\nsubnet $rede netmask $mask {\nrange $rangec $ranget;\n}" >> /etc/dhcp/dhcpd.conf
-	clear
-	/etc/init.d/isc-dhcp-server restart
-	clear
-	/etc/init.d/isc-dhcp-server status
-	Menu
 }	
-
-Reserva(){
-	echo -e '\033[05;31mCRIANDO RESERVAS\033[00;37m'   
-	echo "Diga o MAC: "
-	read mac
-	if [[ $mac =~ ^[0-9A-Fa-f]{12}$ ]]
-	then
-		echo -e "MAC Valido"
-	else
-		clear
-		echo -e "MAC Invalido"
-		Reserva
-	fi
-	mac=$(echo $mac | sed -e 's!\.!!g;s!\(..\)!\1:!g;s!:$!!' -e 'y/abcdef/ABCDEF/')
-	echo  "Diga o nome do HOST: "
-	read host
-	echo "Diga o IP: "
-	read ip
-	echo -e "\nhost $host {\nhardware ethernet $mac;\nfixed-address $ip;\n}" >> /etc/dhcp/dhcpd.conf
-	/etc/init.d/isc-dhcp-server restart
-	clear
-	/etc/init.d/isc-dhcp-server status
-	Menu
-}		
 Menu
