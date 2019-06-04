7b43c3e03850887f10fa357fd756a2062a360c4e
Karan Luciano <karanluciano1@gmail.com>
2019-06-03 22:24:28 -0400

    samba

---
+++ Samba/README.md
@@ -0,0 +1,25 @@
+
+# SAMBA 
+
+This script adds a new path to samba sharing besides adding a new user or setting the old user password (samba user).
+
+# What do you need to use this software?
+Only one linux terminal.
+
+# How to use it?
+You will need the "ZENITY"
+
+Zenity is software that allows you to create several types of simple dialogs for interaction with users in a Linux environment, which can be used in shell scripts.
+
+Install Zenity:<br/>
+In debian-based distributions:<br\>
+`$sudo apt install zenity`<br\>
+
+In arch-based distributions:<br\>
+`$sudo pacman -S zenity`<br\>
+
+Execution permission to samba.sh with the command:<br/>
+`$sudo chmod +x samba.sh`<br/>
+
+Then run the program (need root permission):<br/>
+`$sudo ./samba.sh`
--- Samba/samba.sh
+++ Samba/samba.sh
@@ -18,25 +18,25 @@ if [ $_checks != 0 ]; then
 	exit 1
 else
 
-_path=$(zenity --title="PATH" --text "Which path do you want to put?" --entry --width="350" --height="50")
-_name=$(zenity --title="NAME" --text "What is the name of the share?" --entry --width="350" --height="50")
+	_path=$(zenity --title="PATH" --text "Which path do you want to put?" --entry --width="350" --height="50")
+	_name=$(zenity --title="NAME" --text "What is the name of the share?" --entry --width="350" --height="50")
 
-sudo echo -e  "\n[$_name]
-comment = compar
-path = $_path
-browseable = yes
-writable = yes" >> /etc/samba/smb.conf
+	sudo echo -e  "\n[$_name]
+	comment = compar
+	path = $_path
+	browseable = yes
+	writable = yes" >> /etc/samba/smb.conf
 
-zenity --title="SAMBA" --question --text "WANT TO ADD A NEW USER/PASSWORD TO SAMBA?" --width="400" --height="50" && echo $?
+	zenity --title="SAMBA" --question --text "WANT TO ADD A NEW USER/PASSWORD TO SAMBA?" --width="400" --height="50" && echo $?
 
-if [ $? == 0 ]; then
-	_user=$(zenity --title="USER" --text "What is the username?" --entry --width="350" --height="50")
-	zenity --info --text="PLACE THE PASSWORD IN THE TERMINAL" --width="350" --height="50"
-	sudo smbpasswd -a $_user
-	sudo systemctl enable smb nmb
-	sudo systemctl restart smb nmb
-	sudo systemctl status smb nmb
+	if [ $? == 0 ]; then
+		_user=$(zenity --title="USER" --text "What is the username?" --entry --width="350" --height="50")
+		zenity --info --text="PLACE THE PASSWORD IN THE TERMINAL" --width="350" --height="50"
+		sudo smbpasswd -a $_user
+		sudo systemctl enable smb nmb
+		sudo systemctl restart smb nmb
+		sudo systemctl status smb nmb
 
-fi
-zenity --info --title="Changing Path" --text="Path changed to: $_path" --width="200" height="200"
+	fi
+	zenity --info --title="Changing Path" --text="Path changed to: $_path" --width="200" height="200"
 fi
